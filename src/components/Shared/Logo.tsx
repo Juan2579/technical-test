83f761649681ee0f6b3b8c6bf3a1c8d6a0afb3a0
Juan David Gualdron Navarro <juandavidgualdronnavarro@gmail.com>
2024-07-23 21:08:46 -0500

    ADD header design with Logo and LoginButton components

--- src/components/Shared/Logo.tsx
+++ src/components/Shared/Logo.tsx
@@ -3,8 +3,10 @@ import CodeIcon from "@mui/icons-material/Code";
 export const Logo = () => {
   return (
     <div className="flex items-center gap-2">
-      <CodeIcon sx={{ fontSize: 56 }} className="text-primary" />
-      <span className="text-xl font-bold">DevsPost</span>
+      <CodeIcon className="text-primary text-3xl md:text-5xl" />
+      <span className="text-secondary text-lg font-bold md:text-xl">
+        DevsPost
+      </span>
     </div>
   );
 };

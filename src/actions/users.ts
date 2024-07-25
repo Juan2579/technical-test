a0911b4d76b22848f187ec15665e686d46f91529
Juan David Gualdron Navarro <juandavidgualdronnavarro@gmail.com>
2024-07-25 00:25:01 -0500

    ADD authentication feature

---
+++ src/actions/users.ts
@@ -0,0 +1,48 @@
+"use server";
+
+import { createClient } from "@/utils/supabase/server";
+import { Provider } from "@supabase/supabase-js";
+
+export const loginAction = async (provider: Provider) => {
+  try {
+    const supabase = createClient();
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider,
+      options: {
+        redirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth`,
+      },
+    });
+
+    if (error) throw error;
+
+    return { errorMessage: null, url: data.url };
+  } catch (error) {
+    return { errorMessage: "Error logging in" };
+  }
+};
+
+export const logoutAction = async () => {
+  try {
+    const supabase = createClient();
+    const { error } = await supabase.auth.signOut();
+
+    if (error) return { errorMessage: error.message };
+
+    return { errorMessage: null };
+  } catch (error) {
+    return { errorMessage: "Error logging out" };
+  }
+};
+
+export const getUser = async () => {
+  try {
+    const supabase = createClient();
+    const { data, error } = await supabase.auth.getUser();
+
+    if (error) return { errorMessage: error.message };
+
+    return { errorMessage: null, user: data.user };
+  } catch (error) {
+    return { errorMessage: "Error getting user" };
+  }
+};
---
+++ src/components/Shared/AccountMenu.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import { MouseEvent, useState, useTransition } from "react";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import Logout from "@mui/icons-material/Logout";
+import { User } from "@supabase/auth-js";
+import { logoutAction } from "@/actions/users";
+import { Loader } from "./Loader";
+
+export default function AccountMenu({ user }: { user: User }) {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [isPending, startTransition] = useTransition();
+
+  const open = Boolean(anchorEl);
+
+  const handleOpenAccountMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleCloseAccountMenu = () => {
+    setAnchorEl(null);
+  };
+
+  const handleClickLogoutButton = () => {
+    startTransition(async () => {
+      const { errorMessage } = await logoutAction();
+
+      if (errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  };
+
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          textAlign: "center",
+          gap: "8px",
+        }}
+      >
+        <Tooltip title="Account">
+          <IconButton
+            onClick={handleOpenAccountMenu}
+            size="small"
+            sx={{ ml: 2 }}
+          >
+            {user.user_metadata["avatar_url"] ? (
+              <Avatar
+                sx={{ width: 48, height: 48 }}
+                src={user.user_metadata["avatar_url"]}
+                alt={`${user.user_metadata["user_name"]} avatar`}
+              />
+            ) : (
+              <Avatar
+                className="bg-primary text-white"
+                sx={{ width: 48, height: 48 }}
+              >
+                {user.user_metadata["user_name"].charAt(0).toUpperCase()}
+              </Avatar>
+            )}
+          </IconButton>
+        </Tooltip>
+      </Box>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleCloseAccountMenu}
+        onClick={handleCloseAccountMenu}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+            mt: 1.5,
+            "& .MuiAvatar-root": {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            "&::before": {
+              content: '""',
+              display: "block",
+              position: "absolute",
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <MenuItem className="pointer-events-none">
+          Username: {user.user_metadata["user_name"]}
+        </MenuItem>
+        <MenuItem onClick={handleClickLogoutButton}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+      <Loader open={isPending} />
+    </>
+  );
+}
